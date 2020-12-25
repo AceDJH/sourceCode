2304b19fd8eca1e4bf6803d8d6870326e107847c
AceDJH <460745177@qq.com>
2020-12-25 21:43:11 +0800

    初始化源码学习项目

---
+++ spring-source/src/test/java/com/djh/springsource/SpringSourceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.djh.springsource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
