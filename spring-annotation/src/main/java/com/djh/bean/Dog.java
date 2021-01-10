169d2d36a1c0e0e32d01184dca6c818d10af9cad
AceDJH <460745177@qq.com>
2021-01-10 23:33:14 +0800

    生命周期--BeanPostProcessor在Spring底层使用

--- spring-annotation/src/main/java/com/djh/bean/Dog.java
+++ spring-annotation/src/main/java/com/djh/bean/Dog.java
@@ -1,5 +1,10 @@
 package com.djh.bean;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanInitializationException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -10,7 +15,8 @@ import javax.annotation.PreDestroy;
  * @Date 2021/1/6 17:48
  */
 @Component
-public class Dog {
+public class Dog implements ApplicationContextAware {
+    private ApplicationContext applicationContext;
     public Dog(){
         System.out.println("dog constructor...");
     }
@@ -25,5 +31,24 @@ public class Dog {
     @PreDestroy
     public void destroy(){
         System.out.println("Dog @PreDestroy...");
+    }
+
+    /**
+     * Set the ApplicationContext that this object runs in.
+     * Normally this call will be used to initialize the object.
+     * <p>Invoked after population of normal bean properties but before an init callback such
+     * as {@link InitializingBean#afterPropertiesSet()}
+     * or a custom init-method. Invoked after {@link ResourceLoaderAware#setResourceLoader},
+     * {@link ApplicationEventPublisherAware#setApplicationEventPublisher} and
+     * {@link MessageSourceAware}, if applicable.
+     *
+     * @param applicationContext the ApplicationContext object to be used by this object
+     * @throws ApplicationContextException in case of context initialization errors
+     * @throws BeansException              if thrown by application context methods
+     * @see BeanInitializationException
+     */
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
